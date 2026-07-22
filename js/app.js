0d756166c06e7cd685198cda1e74870304093772
Ananthu Unnithan <unnithanananthu69@gmail.com>
2026-07-22 09:42:48 +0530

    little css added

--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
-var app= angular.module('campus-360',[]);
+var app = angular.module("campus-360", []);
 
-app.controller("dashboardcontroller",function($scope){
-
+app.controller("dashboardcontroller", function ($scope) {
 
     $scope.collegename = "FISAT";
     $scope.totalStudents = 1200;
     $scope.totalFaculty = 85;
 
-    console.log("dashboard controller is loaded");
+    console.log("Dashboard Controller Loaded");
 
-    $scope.students=[
-        "Shahid","John","Allen","Shaun"
+    $scope.students = [
+        "Shahid",
+        "John",
+        "Allen",
+        "Shaun"
     ];
 
-    $scope.addStudent=function (){
+    $scope.showStudents = true;
+    $scope.showDashboard = true;
+
+    // Add Student
+    $scope.addStudent = function () {
+        $scope.students.push("New Student");
         $scope.totalStudents++;
-    }
+    };
+
+    // Add Faculty
+    $scope.addFaculty = function () {
+        $scope.totalFaculty++;
+    };
+
 });
